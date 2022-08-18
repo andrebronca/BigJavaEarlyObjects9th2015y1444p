e9ac2aa7416776a3e833b4bd1429c4211a23dd94
André R. B <bronca.andre@gmail.com>
2022-08-18 18:54:38 -0300

    inicio

---
+++ src/com/arb/chapter01/HelloPrinter.java
@@ -0,0 +1,8 @@
+package com.arb.chapter01;
+
+//fazer de conta que nunca nem vi
+public class HelloPrinter {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
